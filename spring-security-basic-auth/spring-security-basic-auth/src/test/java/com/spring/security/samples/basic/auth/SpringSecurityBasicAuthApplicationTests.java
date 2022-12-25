48e7c7b2e603c7004b36f4139c15749739b84ae3
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-25 20:48:46 -0300

    Criação de projeto spring boot

---
+++ spring-security-basic-auth/spring-security-basic-auth/src/test/java/com/spring/security/samples/basic/auth/SpringSecurityBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.samples.basic.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
