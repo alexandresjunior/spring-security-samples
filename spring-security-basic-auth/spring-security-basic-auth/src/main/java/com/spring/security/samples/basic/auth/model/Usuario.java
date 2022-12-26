9d1ceb3698412b6cd0418e59db075624be984e01
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-25 21:18:47 -0300

    Criação da camada model

---
+++ spring-security-basic-auth/spring-security-basic-auth/src/main/java/com/spring/security/samples/basic/auth/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.spring.security.samples.basic.auth.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+}
